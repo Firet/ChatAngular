d652bd1a2b9167113b7f484b57f8e49f9f05f931
Joaquin Luna <joaquinluna@iPad-de-DJAA.localdomain>
2018-06-15 11:15:07 -0300

    clase 1 angular - scope

--- js/controllers/homeCtrl.js
+++ js/controllers/homeCtrl.js
@@ -1,4 +1,16 @@
 angular.module("primerProyecto")
 .controller('homeCtrl', function($scope) {
-
+    var nombre = "joa";
+    let contactos = [{
+        nombre: "damian"
+    },{
+        nombre: nombre
+    },{
+        nombre: "pepe"
+    }];
+    
+    $scope.usuarios = contactos;
+    $scope.mostrarNombre = function(index){
+        alert(contactos[index].nombre);
+    }
 });
